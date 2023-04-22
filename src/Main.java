38bf2ca5c159deee15c1a306c0a1ca89544c8727
Guilherme Tito <guilhermeferrazronha123@gmail.com>
2023-04-22 15:21:03 -0300

    implementei todos até o quickSort

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,13 @@ public class Main
                 case 2 -> selectionSort(vetor);
                 case 3 -> bubbleSort(vetor);
                 case 4 -> combSort(vetor);
-                case 5 -> quickSort(vetor);
+                case 5 ->
+                {
+                    System.out.println("Quick Sort");
+                    gerarVetor(vetor);
+                    quickSort(vetor, 0, vetor.length - 1);
+                    imprimirVetor(vetor);
+                }
                 case 6 -> mergeSort(vetor);
                 case 7 -> heapSort(vetor);
                 case 8 -> shellSort(vetor);
@@ -53,14 +59,23 @@ public class Main
     public static void imprimirVetor(int[] vetor)
     {
         for (int i : vetor) {
-            System.out.println("[" + i + "]");
+            if(i < 10)
+            {
+                System.out.println("[0" + i + "]");
+            }
+            else
+            {
+                System.out.println("[" + i + "]");
+            }
         }
     }
 
     public static int entradaInteira()
     {
         Scanner leitor = new Scanner(System.in);
+
         boolean ime = true; //InputMismatchException
+
         while(ime)
         {
             if(leitor.hasNextInt())
@@ -79,19 +94,57 @@ public class Main
     public static void insertionSort(int[] vetor)
     {
         System.out.println("Insertion Sort");
+
+        int n = vetor.length;
+
+        for(int i = 0; i < n; i++)
+        {
+            int j = i;
+
+            while(j > 0 && vetor[j - 1] > vetor[j])
+            {
+                int aux = vetor[j];
+                vetor[j] = vetor[j - 1];
+                vetor[j - 1] = aux;
+                j--;
+            }
+        }
+        imprimirVetor(vetor);
     }
 
     public static void selectionSort(int[] vetor)
     {
         System.out.println("Selection Sort");
+
+        int n = vetor.length;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            int min = i;
+
+            for(int j = i; j < n; j++)
+            {
+                if(vetor[j] < vetor[min])
+                {
+                    min = j;
+                }
+            }
+            int aux = vetor[i];
+            vetor[i] = vetor[min];
+            vetor[min] = aux;
+        }
+        imprimirVetor(vetor);
     }
 
     public static void bubbleSort(int[] vetor)
     {
         System.out.println("Bubble Sort");
-        for(int i = 0; i < vetor.length - 1; i++)
+
+        int n = vetor.length;
+
+        for(int i = 0; i < n - 1; i++)
         {
-            for(int j = i + 1; j < vetor.length; j++)
+            for(int j = i + 1; j < n; j++)
             {
                 if(vetor[j] < vetor[i])
                 {
@@ -107,11 +160,74 @@ public class Main
     public static void combSort(int[] vetor)
     {
         System.out.println("Comb Sort");
+
+        int n = vetor.length;
+        int intervalo = n;
+        boolean trocou = true;
+
+        while(intervalo != 1 || trocou) //se o intervalo for 1 e nenhuma troca for feita, o vetor está ordenado
+        {
+            intervalo = (intervalo * 10) / 13;
+
+            if(intervalo < 1)
+            {
+                intervalo = 1;
+            }
+
+            trocou = false;
+
+            for(int i = 0; i < n - intervalo; i++)
+            {
+                if(vetor[i + intervalo] < vetor[i])
+                {
+                    int aux = vetor[i];
+                    vetor[i] = vetor[i + intervalo];
+                    vetor[i + intervalo] = aux;
+
+                    trocou = true;
+                }
+            }
+        }
+        imprimirVetor(vetor);
     }
 
-    public static void quickSort(int[] vetor)
+    public static int particao(int[] vetor, int inicio, int fim)
     {
-        System.out.println("Quick Sort");
+        int ref = vetor[inicio];
+        int down = inicio;
+        int up = fim;
+        while(down < up)
+        {
+            //encontrar um valor maior que o valor ref (pivô)
+            while(vetor[down] <= ref && down < fim)
+            {
+                down++;
+            }
+            //encontrar um valor menor que o valor ref (pivô)
+            while(vetor[up] > ref)
+            {
+                up--;
+            }
+            //fazer a troca dos valores se a posição down for menor que up, ou seja, se os índices não se cruzaram
+            if(down < up)
+            {
+                int aux = vetor[down];
+                vetor[down] = vetor[up];
+                vetor[up] = aux;
+            }
+        }
+        //
+        vetor[inicio] = vetor[up];
+        vetor[up] = ref;
+        return up; //up agora é a posição correta do pivô
+    }
+    public static void quickSort(int[] vetor, int inicio, int fim)
+    {
+        int pivo;
+        if(inicio >= fim) return;
+        pivo = particao(vetor, inicio, fim);
+        quickSort(vetor, inicio, pivo - 1);
+        quickSort(vetor, pivo + 1, fim);
     }
 
     public static void mergeSort(int[] vetor)
